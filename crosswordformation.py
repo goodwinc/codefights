fa0eda0d545cb47b368f8e4ff8e50e6105dd1dab
Goodwin Chen <goodwinc@gmail.com>
2017-01-21 14:14:27 -0500

    added daren's suggestions

--- crosswordformation.py
+++ crosswordformation.py
@@ -5,31 +5,32 @@ Created on Fri Jan 20 22:00:44 2017
 
 @author: goodwin
 """
+import itertools
+
 
 def crosswordFormation(words):
     count = 0
-    for north in words:
-        words1 = words[:]
-        words1.remove(north)
-        for east in words1:
-            words2 = words1[:]
-            words2.remove(east)
-            for south in words2:
-                words3 = words2[:]
-                words3.remove(south)
-                for west in words3:
-                    for i in range(2, len(north)):
-                        for j in range(len(east)-2):
-                            if north[i] == east[j]:
-                                for k in range(i-1):
-                                    for l in range(len(west)-2):
-                                        if north[k] == west[l]:
-                                            for m in range(l+2, len(west)):
-                                                for n in range(len(south)-(i-k)):
-                                                    try:
-                                                        if (west[m] == south[n] and
-                                                            south[n+i-k] == east[j+m-l]):
-                                                                count+=1
-                                                    except IndexError:
-                                                        pass
+    perms = itertools.permutations(words)
+    for perm in perms:
+        north = perm[0]
+        east = perm[1]
+        west = perm[2]
+        south = perm[3]
+        for i in range(2, len(north)):
+            for j in range(len(east)-2):
+                if north[i] == east[j]:
+                    for k in range(i-1):
+                        for l in range(len(west)-2):
+                            if north[k] == west[l]:
+                                for m in range(l+2, len(west)):
+                                    if j+m-l < len(east):
+                                        for n in range(len(south)-(i-k)):
+                                            if n+i-k < len(south):
+                                                if west[m] == south[n]:
+                                                    if south[n+i-k] == east[j+m-l]:
+                                                        count+=1
+                                            else:
+                                                break
+                                    else:
+                                        break
     return count
